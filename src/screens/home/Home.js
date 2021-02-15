5bf4bdb2939f020cfe78bde67e581766e6f14c22
Kavya-Gowtham <kavyanagaraj20596@gmail.com>
2021-02-15 18:03:30 +0530

    Added 'Apply' button for movie filters on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -18,7 +18,8 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemText from '@material-ui/core/ListItemText';
-import TextField from "@material-ui/core/TextField";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
     root: {
@@ -68,6 +69,7 @@ class Home extends Component {
     artistSelectorHandler = event => {
         this.setState({ artists: event.target.value });
     }
+
     render() {
         const { classes } = this.props;
         return (
@@ -143,9 +145,9 @@ class Home extends Component {
                                     >
                                         <MenuItem value='0'>None</MenuItem>
                                         {artists.map(artist => (
-                                            <MenuItem key={artist.id} value={artist.first_name+ " " + artist.last_name}>
-                                                <Checkbox checked={this.state.artists.indexOf(artist.first_name+ " " + artist.last_name) > -1} />
-                                                <ListItemText primary={artist.first_name+ " " + artist.last_name} />
+                                            <MenuItem key={artist.id} value={artist.first_name + " " + artist.last_name}>
+                                                <Checkbox checked={this.state.artists.indexOf(artist.first_name + " " + artist.last_name) > -1} />
+                                                <ListItemText primary={artist.first_name + " " + artist.last_name} />
                                             </MenuItem>
                                         ))}
                                     </Select>
@@ -153,22 +155,26 @@ class Home extends Component {
 
                                 <FormControl className={classes.formControl}>
                                     <TextField
-                                    id="releaseDateStart"
-                                    label="Release Date Start"
-                                    type="date"
-                                    defaultValue=""
-                                    InputLabelProps={{shrink: true}}
+                                        id="releaseDateStart"
+                                        label="Release Date Start"
+                                        type="date"
+                                        defaultValue=""
+                                        InputLabelProps={{ shrink: true }}
                                     />
                                 </FormControl>
 
                                 <FormControl className={classes.formControl}>
                                     <TextField
-                                    id="releaseDateEnd"
-                                    label="Release Date End"
-                                    type="date"
-                                    defaultValue=""
-                                    InputLabelProps={{shrink: true}}
+                                        id="releaseDateEnd"
+                                        label="Release Date End"
+                                        type="date"
+                                        defaultValue=""
+                                        InputLabelProps={{ shrink: true }}
                                     />
+                                </FormControl>
+                                <br/><br/>
+                                <FormControl className={classes.formControl}>
+                                    <Button variant="contained" color="primary">Apply</Button>
                                 </FormControl>
                             </CardContent>
                         </Card>
